1ac5ec4ca770654d6fb3981adde3796666ad8f3a
Brandon Martinez <brandmartinez96@gmail.com>
2021-11-19 21:10:34 -0300

    se agrega componente de login y router

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 import { ClientDetalleComponent } from './components/client/client-detalle/client-detalle.component';
 import { ClientUpdateComponent } from './components/client/client-update/client-update.component';
 import { ClientListComponent } from './components/client/client-list/client-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
 // Importar Rutas
 import { ROUTES } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,10 +26,12 @@ import { ROUTES } from './app.routes';
     ClientDetalleComponent,
     ClientUpdateComponent,
     ClientListComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot( ROUTES, { useHash: true } )
   ],
   providers: [],
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,10 +2,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ClientDetalleComponent } from './components/client/client-detalle/client-detalle.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const ROUTES: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'client/:id', component: ClientDetalleComponent},
+    {path: 'login', component: LoginComponent},
     {path: '', pathMatch: 'full', redirectTo : 'home'},
     {path: '**', pathMatch: 'full', redirectTo : 'home'}
 ];
