20ab588dc66a1ba0a063471a8adbc2ac31ac419f
Joseph896 <josephramos896@gmail.com>
2021-11-25 22:30:06 -0300

    Creando los DTOs's

---
+++ Api/CommonSolution/Dtos/ClienteDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonSolution.Dtos
+{
+    public class ClienteDto
+    {
+        public string Documento { get; set; }
+        public string Telefono { get; set; }
+        public int Tipo_documento { get; set; }
+        public string Localidad { get; set; }
+        public string Calle { get; set; }
+        public string Detalle_direccion { get; set; }
+        public string Email { get; set; }
+        public int id_condado { get; set; }
+
+        public virtual CondadoDto Condado { get; set; }
+        public virtual EmpresaDto Empresa { get; set; }
+        public virtual PersonaDto Persona { get; set; }
+    }
+}
