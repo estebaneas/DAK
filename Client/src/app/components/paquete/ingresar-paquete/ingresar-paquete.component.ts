6838a6f75fcbacee1ad4d0774b54fe5222148fd1
Brandon Martinez <brandmartinez96@gmail.com>
2021-11-28 03:29:42 -0300

    Validaciones para ingresar paquete front

--- Client/src/app/components/paquete/ingresar-paquete/ingresar-paquete.component.ts
+++ Client/src/app/components/paquete/ingresar-paquete/ingresar-paquete.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { CondadoService } from 'src/app/services/dak/condado/condado.service';
 
 @Component({
@@ -19,6 +20,50 @@ export class IngresarPaqueteComponent{
       this.loading = false;
     });
    }
+
+   formPaquete = new FormGroup(
+    {
+      remitente: new FormControl('', [Validators.required]),
+      destinatario: new FormControl('', [Validators.required]),
+      calle: new FormControl('', [Validators.required]),
+      condado: new FormControl('', [Validators.required]),
+      localidad: new FormControl('', [Validators.required]),
+      detalle: new FormControl('', [Validators.required]),
+      peso: new FormControl('', [Validators.required])
+    }
+  )
+
+    //#region Validaciones
+    get remitente()
+    {
+      return this.formPaquete.get('remitente');
+    }
+  
+    get destinatario()
+    {
+      return this.formPaquete.get('destinatario');
+    }
+    get calle()
+    {
+      return this.formPaquete.get('calle');
+    }
+    get condado()
+    {
+      return this.formPaquete.get('condado');
+    }
+    get localidad()
+    {
+      return this.formPaquete.get('localidad');
+    }
+    get detalle()
+    {
+      return this.formPaquete.get('detalle');
+    }
+    get peso()
+    {
+      return this.formPaquete.get('peso');
+    }
+    //#endregion
 
 
 }
