891d0a7bca3fc1fdb6d7edb7f2445fa4647da048
Brandon Martinez <brandmartinez96@gmail.com>
2021-11-29 01:36:29 -0300

    Revert "Se agrega el numero de tracking al paquete"
    This reverts commit 1f1d32d604bb61de317f5101decb5f5578700a7a.

--- Api/Services/PaqueteService.cs
+++ Api/Services/PaqueteService.cs
@@ -5,23 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TrackingMoule.Bussiness;
 
 namespace Services
 {
     public class PaqueteService
     {
         private readonly PaqueteRepository _paqueteRepository;
-        private readonly TrackingBusiness _trackingBusiness;
-        
         public PaqueteService()
         {
             this._paqueteRepository = new PaqueteRepository();
         }
         public PaqueteDto AltaPaquete(PaqueteDto paquete)
         {
-            string numeroTracking = _trackingBusiness.SolicitarNuevoTracking();
-            paquete.TrackingNumero = numeroTracking;
             return this._paqueteRepository.registrarPaquete(paquete);
         }
     }
