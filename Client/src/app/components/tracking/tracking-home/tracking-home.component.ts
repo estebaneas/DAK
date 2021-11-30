1338db5fd18df5f78d8a575f8ed53e4c03a2d474
Emiliano Alvez <esteban.emiliano.alvez.saez@gmail.com>
2021-11-30 15:06:42 -0300

    Agregado al front google maps component Actualziado el modelo para agregarle el numero de trackign al paquete

--- Client/src/app/components/tracking/tracking-home/tracking-home.component.ts
+++ Client/src/app/components/tracking/tracking-home/tracking-home.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { TrackingService } from 'src/app/services/dak/tracking/tracking.service';
+import { coordenadas } from '../../models/coordenadas';
+interface pokemon {
+  id:number;
+  name:string;
+}
 @Component({
   selector: 'app-tracking-home',
   templateUrl: './tracking-home.component.html',
@@ -8,16 +15,35 @@ import { FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class TrackingHomeComponent implements OnInit {
 
-  constructor() { }
-
-  formTracking= new FormGroup({
+  constructor(private service: TrackingService , private route:Router) { }
+  public formTracking= new FormGroup({
     numeroTracking : new FormControl('',[Validators.required])
   })
 
   ngOnInit(): void {
   }
 
-  verificar(){
+  async verificar() {
+    var tracking = this.formTracking.value['numeroTracking']
+    try{
+      const respuesta = await (await this.service.verificar(tracking)).json();
+      if(respuesta){
+        this.mostrarhubicacion(tracking);
+      }
+      else{
+        console.log("Ese numero no existe");
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
 
+  async mostrarhubicacion(tracking:string)
+  {
+    const coordenadas =  <coordenadas> await (await this.service.cargarHubicacion(tracking)).json();
+    sessionStorage.setItem("coords",JSON.stringify(coordenadas))
+    this.route.navigateByUrl('tracking-results');
+    
+    this.service.puente.emit(coordenadas);
   }
 }
