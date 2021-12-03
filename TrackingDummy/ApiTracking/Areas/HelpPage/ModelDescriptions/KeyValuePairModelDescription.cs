731679b0dbb88274562070dc83aeb824be824ac5
Emiliano Alvez <esteban.emiliano.alvez.saez@gmail.com>
2021-12-03 12:10:11 -0300

    Añadida la carpeta con el Dummy de tracking externo

---
+++ TrackingDummy/ApiTracking/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ApiTracking.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
